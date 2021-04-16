1726e63bceb54e7b2da2ffa2652196acd979dea0
Julian Bolanos <julian.camilob24@gmail.com>
2021-04-16 11:07:50 -0500

    The program is able to solve a tower of hanoi for 1 case.

--- src/model/TowerOfHanoi.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class TowerOfHanoi {
-
-	public TowerOfHanoi() {
-	}
-
-}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -8,15 +8,18 @@ import java.util.ArrayList;
 public class Main {
 	
 	public final static String INPUT_PATH = "data/input.txt";
-
-	private ArrayList<Integer> towerA;
-	private ArrayList<Integer> towerB;
-	private ArrayList<Integer> towerC;
+	
+	private int[] towers;
+	
+	private int positionA = 0; 
+	private int positionB = 1; 
+	private int positionC = 2; 
 	
 	private String solutions;
 	
 	public Main() {
 		solutions = "";
+		towers = new int[3];
 	}
 
 	public static void main(String[] args) {
@@ -27,6 +30,7 @@ public class Main {
 			for (Integer problem : problems) {
 				app.getSolutionTowerHanoi(problem);
 				System.out.println(app.getSolutions());
+				
 			}
 			
 			
@@ -40,7 +44,7 @@ public class Main {
 		
 		BufferedReader br = new BufferedReader(new FileReader(INPUT_PATH));
 		
-		//Reads amount of problems
+		//Reads amount of problems and problems
 		br.readLine();
 		ArrayList<Integer> problems = new ArrayList<Integer>();
 		
@@ -57,59 +61,69 @@ public class Main {
 	
 	
 	public void getSolutionTowerHanoi(int disks) {
-		towerA = new ArrayList<Integer>();
-		for (int i = disks; i > 0; i--) {
-			towerA.add(i);
-		}
-		towerB = new ArrayList<Integer>();
-		towerC = new ArrayList<Integer>();
-		solutions += getTowers();
-		getSolutionTowerHanoi(disks, towerA, towerC, towerB);
-		solutions += getTowers();
+		
+		//Setup the towers
+		towers[positionA] = disks;
+		
+		//Get solution of towers
+		getTowers();
+		getSolutionTowerHanoi(disks, positionA, positionC, positionB);
+		solutions += "\n";
 	}
 
-	private void getSolutionTowerHanoi(int disks, ArrayList<Integer> origin, ArrayList<Integer> goal, ArrayList<Integer> temporal) {
+	private void getSolutionTowerHanoi(int disks, int origin, int goal, int temporal) {
 		
-		//Imprime parámetros de entrada
-		/*System.out.println("param:" + "\n" + 
+		/*
+		//parámetros de entrada
+		System.out.println("param:" + "\n" + 
 							"disks = " + disks + "\n" + 
 							"origin = " + origin + "\n" + 
 							"goal = " + goal + "\n" +
-							"temporal = " + temporal + "\n");*/
+							"temporal = " + temporal + "\n");
 		if(disks == 1) {
 			goal.add(origin.remove(origin.size() - 1));
-			
-			//parámetros después del caso base
-			/*System.out.println("step BASE OVER" + "\n" + "POST BASE \n" +
+			solutions += origin.size() + " " + temporal.size() + " " + goal.size() + "\n"; //+ " PASO BASE";
+			//estado de los parámetros después del caso base
+			System.out.println("PASO BASE ACABADO" + "\n" + "POST BASE \n" +
 					"origin = " + origin + "\n" + 
 					"goal = " + goal + "\n" +
 					"temporal = " + temporal + "\n");
-			*/
+			
 		}else {
 			
 			//PASO 1
 			getSolutionTowerHanoi(disks - 1, origin, temporal, goal);
-			solutions += origin.size() + " " + temporal.size() + " " + goal.size() + "\n"; //+ " PASO BASE";
-			//solutions += " PASO 1 \n";
-			//System.out.println("PASO 1 ACABADO");
-			
-			//PASO 2
-			goal.add(origin.remove(origin.size() - 1));
-			/*System.out.println("PASO 2 ACABADO" + "\n" + "POST PASO 2 \n" +
+			//solutions += origin.size() + " " + temporal.size() + " " + goal.size() + "\n";  //+ " PASO 1 \n";
+			System.out.println("PASO 1 ACABADO" + "\n" + "POST PASO 1 \n" +
 					"origin = " + origin + "\n" + 
 					"goal = " + goal + "\n" +
 					"temporal = " + temporal + "\n");
-			*/
+			
+			//PASO 2
+			goal.add(origin.remove(origin.size() - 1));
+			System.out.println("PASO 2 ACABADO" + "\n" + "POST PASO 2 \n" +
+					"origin = " + origin + "\n" + 
+					"goal = " + goal + "\n" +
+					"temporal = " + temporal + "\n");
+			
 			solutions += origin.size() + " " + temporal.size() + " " + goal.size() + "\n"; //" PASO 2" + "\n";
 			
 			//PASO 3
 			getSolutionTowerHanoi(disks - 1, temporal, goal, origin);
 			//solutions += " PASO 3 \n";
-			/*System.out.println("PASO 3 OVER" + "\n" + "POST PASO 3 \n" +
+			System.out.println("PASO 3 ACABADO" + "\n" + "POST PASO 3 \n" +
 					"origin = " + origin + "\n" + 
 					"goal = " + goal + "\n" +
 					"temporal = " + temporal + "\n");
-			*/
+			
+		}*/
+		
+		if(disks > 0) {
+			getSolutionTowerHanoi(disks - 1, origin, temporal, goal);
+			towers[origin]--;
+			towers[goal]++;
+			getTowers();
+			getSolutionTowerHanoi(disks - 1, temporal, goal, origin);
 		}
 
 	}
@@ -118,8 +132,8 @@ public class Main {
 		return solutions;
 	}
 	
-	public String getTowers() {
-		return towerA.size() + " " + towerB.size() + " " + towerC.size() + "\n";
+	public void getTowers() {
+		solutions += towers[positionA] + " " + towers[positionB] + " " + towers[positionC] + "\n";
 	}
 	
 
